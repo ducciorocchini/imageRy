4924022313fea7b1161c81740422accdddeff16a
Duccio Rocchini <ducciorocchini@gmail.com>
2023-07-06 15:38:59 +0200

    Update im.ggplot.R

--- R/im.ggplot.R
+++ R/im.ggplot.R
@@ -1,7 +1,6 @@
 # Function to plot outputs with ggplot2 and vridis
 # requires ggplot2 and viridis packages
-im.ggplot <- function(m){
+im.ggplot <- function(m, layerfill){
 md <- as.data.frame(m, xy=T)
-layer <- names(m)[[1]]
-ggplot() + geom_raster(md, mapping = aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option='viridis')
+ggplot() + geom_raster(md, mapping = aes(x=x, y=y, fill=layerfill)) + scale_fill_viridis(option='viridis')
 }
