1caedddf3ea41fc3fd64acc0fe2d865973279560
Duccio Rocchini <ducciorocchini@gmail.com>
2023-07-06 14:59:40 +0200

    Update im.pca.R

--- R/im.pca.R
+++ R/im.pca.R
@@ -2,13 +2,10 @@ im.pca <- function(x){
 # random sample
 set.seed(1)
 sr <- spatSample(x, 10000, method="random")
-  
 # principal component
 pca <- prcomp(sr)
-
 # variance explained
 summary(pca)
-
 # pc map
 pci <- predict(x, pca, index = 1:2)
 }
