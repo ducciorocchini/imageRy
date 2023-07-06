e94a909f295cf36d5924ac057eb36df3e408f469
Duccio Rocchini <ducciorocchini@gmail.com>
2023-07-06 06:31:46 +0200

    Update im.refresh.R

--- R/im.refresh.R
+++ R/im.refresh.R
@@ -1,5 +1,14 @@
+#' im.refresh
+#' 
+#' A function to refresh imageRy
+#'
+#' @refresh imageRy
+#' @export
+#'
+#' @examples
+#' im.refresh()
+
 im.refresh <- function(){
   devtools::install_github("ducciorocchini/imageRy", force=T)
 }
 
-im.refresh()
