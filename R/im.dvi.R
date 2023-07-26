d8c27d586a2820ada3ef53625a0135fe6f72b334
Duccio Rocchini <ducciorocchini@gmail.com>
2023-07-26 07:23:17 +0200

    Update im.dvi.R

--- R/im.dvi.R
+++ R/im.dvi.R
@@ -1,3 +1,17 @@
-im.dvi <- function(x,nir,r){
-x$nir-x$r
+im.ndvi <- function(x, nir, red){
+  
+  if(!inherits(x, "SpatRaster")) {
+    stop("Input image should be a SpatRaster object.")
+  }
+  
+  if(!inherits(nir, "numeric") && !inherits(red, "numeric")) {
+    stop("NIR and red layers should be indicated with a number")
+  }
+  
+  ndvi <- (x[[nir]] - x[[red]]) / (x[[nir]] + x[[red]])
+  print(im.ggplot(ndvi))
+  
+  return(ndvi)
+
 }
+
