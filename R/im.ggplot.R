95ad938ed8d4d2de0e60ddca0fbd6eb98208e3e9
Duccio Rocchini <ducciorocchini@gmail.com>
2023-07-26 07:26:21 +0200

    Update im.ggplot.R

--- R/im.ggplot.R
+++ R/im.ggplot.R
@@ -1,8 +1,19 @@
-im.ggplot <- function(input_raster, layerfill) {
+im.ggplot <- function(input_raster, layerfill = 1) {
   # Convert raster to dataframe and create plot directly
+  if(!inherits(input_raster, "SpatRaster")) {
+    stop("Input image should be a SpatRaster object.")
+  }
+  
+  if(!inherits(layerfill, "numeric")) {
+    stop("Input layer should be indicated with a number")
+  }
+  
+  df <- as.data.frame(input_raster, xy = TRUE)
+  layerfill <- colnames(df)[layerfill + 2]
+  
   print(
-    ggplot(as.data.frame(input_raster, xy=TRUE), 
-           aes(x=x, y=y, fill=!!sym(layerfill))) +
+    ggplot(df, 
+           aes(x = x, y = y, fill = !!sym(layerfill))) +
       geom_raster(interpolate = TRUE) +
       scale_fill_viridis(option = 'viridis')
   )
