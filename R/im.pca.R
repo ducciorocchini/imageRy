03dabc9cdd4efba6cba0ec791f4df2e30dda6637
Duccio Rocchini <ducciorocchini@gmail.com>
2023-07-26 07:27:02 +0200

    Update im.pca.R

--- R/im.pca.R
+++ R/im.pca.R
@@ -1,12 +1,26 @@
 im.pca <- function(x){
-# random sample
-set.seed(1)
-sr <- spatSample(x, 10000, method="random")
-# principal component
-pca <- prcomp(sr)
-# variance explained
-summary(pca)
-# pc map
-pci <- predict(x, pca, index = 1:2)
-plot(pci[[1]])
+  
+  if(!inherits(x, "SpatRaster")) {
+    stop("Input image should be a SpatRaster object.")
+  }
+  
+  # random sample
+  df <- as.data.frame(x)
+  set.seed(1)
+  sr <- sample(1:nrow(df), 10000)
+  
+  # principal component
+  pca <- prcomp(df[sr,])
+  
+  # variance explained
+  summary(pca)
+  
+  # pc map
+  pci <- predict(x, pca, index = 1:2)
+  plot(pci[[1]])
+  
+  return(pci)
+  
 }
+
+
