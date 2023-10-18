54ed88d0aacda61382c3aba961bc3756e8fc5ba6
Duccio Rocchini <ducciorocchini@gmail.com>
2023-10-18 16:48:43 +0200

    Update im.plotRGB.user.R

--- R/im.plotRGB.user.R
+++ R/im.plotRGB.user.R
@@ -1,5 +1,5 @@
-# im_plotRGB_user: plotRGB choosing bands
+# im.plotRGB: plotRGB choosing bands
 
-im.plotRGB.user <- function(x,r,g,b){
+im.plotRGB <- function(x,r,g,b){
   plotRGB(x,r,g,b,stretch="lin")
 }
