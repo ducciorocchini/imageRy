e979e1e8565e278c83e4ceb39baf8802dc714c5f
Duccio Rocchini <ducciorocchini@gmail.com>
2024-12-13 16:54:46 +0100

    Update im.ridgeline.R

--- R/im.ridgeline.R
+++ R/im.ridgeline.R
@@ -1,18 +1,32 @@
-im.ridgeline <- function(im, scale, option) {
+ Ecco la funzione im.ridgeline() con i nomi delle legende viridis da inserire:
 
+im.ridgeline1 <- function(im, scale, palette = c("viridis", "magma", "plasma", "inferno", "cividis", "mako", "rocket", "turbo")) {
+  
+  palette <- palette[1]
+  
   #Checking inputs
   if(!is(im, "SpatRaster")) stop("im must be a SpatRaster")
   if(!is.numeric(scale)) stop("scale must be numeric")
-  if(!is.character(option)) stop("ooption must be a character")
-  if(!option %in% c("A", "B", "C", "D", "E", "F", "G", "H")) stop("option must be one of the color ooptions in the viridis pachage (A,B,C,D,E,F,G,H)")
-
+  if(!is.character(palette)) stop("ooption must be a character")
+  if(!palette %in% c("viridis", "magma", "plasma", "inferno", "cividis", "mako", "rocket", "turbo")) stop("option must be one of the color ooptions in the viridis pakage (A,B,C,D,E,F,G,H)")
+  
+  switch(palette, 
+         viridis = 'D',
+         magma = 'A',
+         inferno = 'B',
+         plasma = 'C',
+         cividis = 'E',
+         rocket = 'F',
+         mako = 'G',
+         turbo = 'H')
+  
   #Transforming im in a dataframe
   df <- as.data.frame(im, wide = FALSE)
-
+  
   #Final graph
   pl <- ggplot2::ggplot(df, ggplot2::aes(x = values, y = layer, fill = after_stat(x))) +
     ggridges::geom_density_ridges_gradient(scale = scale, rel_min_height = 0.01) +
-    ggplot2::scale_fill_viridis_c(option = option)
-
+    ggplot2::scale_fill_viridis_c(option = palette)
+  
   return(pl)
 }
