1575073e16238a361b058f679e3a7fc401961ebf
Duccio Rocchini <ducciorocchini@gmail.com>
2024-12-13 17:16:48 +0100

    Update im.plotRGB.auto.R

--- R/im.plotRGB.auto.R
+++ R/im.plotRGB.auto.R
@@ -1,4 +1,7 @@
 # im.plotRGB.auto directly plot any image with the first three bands
-im.plotRGB.auto <- function(x){
+im.plotRGB.auto <- function(x, title = 'Main'){
   plotRGB(x,1,2,3,stretch="lin")
+  par(col.axis="white", col.lab = "white", tck = 0)
+  title(main = title, cex.main = 1.3, line = 2.5)
 }
+
