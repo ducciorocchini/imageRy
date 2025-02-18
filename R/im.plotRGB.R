7d964e95e6fe3973d4a1951e0e31cf7d50b3d75b
Duccio Rocchini <ducciorocchini@gmail.com>
2025-02-18 14:47:53 +0100

    Update im.plotRGB.R

--- R/im.plotRGB.R
+++ R/im.plotRGB.R
@@ -1,5 +1,5 @@
 # im.plotRGB: plotRGB choosing bands
-im.plotRGB <- function(x, r, g, b, title = 'Main'){
+im.plotRGB <- function(x, r, g, b, title = ''){
   par(col.axis="white", col.lab = "white", tck = 0)
   plotRGB(x, r, g, b, stretch = "lin", axes = T, mar = c(1,1,2,1))
   title(main = title, cex.main = 1.3, line = 2.5)
