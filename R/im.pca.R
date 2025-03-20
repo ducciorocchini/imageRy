1186f90e7a40316f60d94945b79584d3504e2369
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-20 17:54:35 +0100

    Update im.pca.R

--- R/im.pca.R
+++ R/im.pca.R
@@ -1,3 +1,42 @@
+#' Perform Principal Component Analysis (PCA) on a Raster Image
+#'
+#' This function applies Principal Component Analysis (PCA) to a multispectral raster image,
+#' extracting all available principal components. It reduces dimensionality while preserving
+#' the most important variance in the dataset.
+#'
+#' @param input_image A `SpatRaster` object representing the input multispectral image.
+#' @param n_samples An integer specifying the number of random samples used for PCA computation (default: 100).
+#'
+#' @return A `SpatRaster` object containing all computed principal components.
+#'
+#' @details
+#' Principal Component Analysis (PCA) is a statistical technique used to transform correlated
+#' raster bands into a set of orthogonal components, capturing the most variance in fewer bands.
+#'
+#' - The function **automatically determines** the number of components based on the number of bands.
+#' - A sample of `n_samples` pixels is used to compute the PCA transformation.
+#' - The **full image** is then projected onto the principal component space.
+#' - The resulting raster contains **all computed principal components**.
+#' - The output is visualized using a `viridis` color scale.
+#'
+#' @seealso [im.import()], [im.ggplot()]
+#'
+#' @examples
+#' \dontrun{
+#' library(terra)
+#' library(viridis)
+#'
+#' # Load a multispectral raster
+#' r <- rast(system.file("ex/logo.tif", package = "terra"))
+#'
+#' # Perform PCA on the raster
+#' pca_result <- im.pca(r, n_samples = 200)
+#'
+#' # Plot the first principal component
+#' plot(pca_result[[1]])
+#' }
+#'
+#' @export
 library(terra)
 library(viridis)
 
