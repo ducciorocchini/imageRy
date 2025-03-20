e29d77af47c1bcaaa80275c69d4153e2af29fcb7
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-20 17:56:38 +0100

    Update im.print.R

--- R/im.print.R
+++ R/im.print.R
@@ -1,5 +1,21 @@
-# im_print.R
-
+#' Print a Message Identifying the imageRy Package
+#'
+#' This function prints a simple message to indicate that the `imageRy` package is being used.
+#'
+#' @return This function does not return an object. It prints a message to the console.
+#'
+#' @details
+#' This function serves as a basic test function to verify that the `imageRy` package is loaded.
+#'
+#' @seealso [im.list()], [im.import()]
+#'
+#' @examples
+#' \dontrun{
+#' # Print the imageRy message
+#' im.print()
+#' }
+#'
+#' @export
 im.print <- function(){
   print("I am imageRy")
   }
