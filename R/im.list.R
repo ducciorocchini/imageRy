59b42ca544c359388375ffcd246a9381e47eea01
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-20 17:52:05 +0100

    Update im.list.R

--- R/im.list.R
+++ R/im.list.R
@@ -1,3 +1,24 @@
+#' List Available Raster Images in the imageRy Package
+#'
+#' This function lists all raster images stored in the `imageRy` package.
+#'
+#' @return A character vector containing the names of available raster image files.
+#'
+#' @details
+#' The function retrieves the names of all files in the "images" directory of the `imageRy` package.
+#' These files can be imported using `im.import()`.
+#'
+#' @seealso [im.import()]
+#'
+#' @examples
+#' \dontrun{
+#' library(imageRy)
+#'
+#' # List available images
+#' im.list()
+#' }
+#'
+#' @export
 im.list <- function() {
   list.files(system.file("images", package="imageRy"))
 }
